760e1e4d1c24a17b24efce7d1d58c4b534bf633e
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-22 13:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	let firstName = document.getElementsByTagName("input")[0];
-	let lastName = document.getElementsByTagName("input")[1];
-	return firstName lastName;
+	let firstName = document.getElementsByTagName("input")[0].value;
+	let lastName = document.getElementsByTagName("input")[1].value;
+	return firstName, lastName;
 }
